d3a6145a22402ed956713f14b3dbf750e3314c2b
jamerrq <jamerrq@gmail.com>
2024-03-04 15:15:58 -0500

    feat: interactive background

---
+++ src/components/global/Background/Opulento.tsx
@@ -0,0 +1,9 @@
+import { Opulento } from "uvcanvas"
+
+export default function Background() {
+    return (
+        <div className="w-[100dvw] h-[100dvh] -z-10 absolute">
+            <Opulento />
+        </div>
+    )
+}
