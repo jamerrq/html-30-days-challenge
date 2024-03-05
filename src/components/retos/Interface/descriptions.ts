f2cf460b16f22db410a7ebff7f652758edcb3faa
jamerrq <jamerrq@gmail.com>
2024-03-05 14:27:47 -0500

    feat: one place for all descriptions

---
+++ src/components/retos/Interface/descriptions.ts
@@ -0,0 +1,7 @@
+export default {
+    1: 'Crea una página HTML con código CSS desde un archivo diferente.',
+    2: 'Crea una página con un titular, varios párrafos de texto y una imagen.',
+    3: 'Construye una página con un párrafo que tenga enlaces internos y externos (a otros sitios web).',
+    4: 'Valida el código HTML de tus ejemplos anteriores (y los siguientes a partir de ahora).',
+    5: 'Ponle un título y una descripción al documento, ideal para SEO.'
+}
